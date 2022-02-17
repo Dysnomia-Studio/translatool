94ac945abf4746bfe4cbeed2119da25608db95c4
Elanis <elanis@hotmail.com>
2022-02-17 19:04:08 +0100

    Fix tests

--- src/components/App/view.test.js
+++ src/components/App/view.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './view';
 
+function useLanguagesListMock() {
+	return {
+		fr: 'Francais',
+		en: 'English',
+	};
+}
+
 test('renders root component without error', () => {
-	render(<App />);
+	render(<App
+		useLanguagesList={() => useLanguagesListMock()}
+	/>);
 	//const textElement = screen.getByText(/Empty text/i);
 	//expect(textElement).toBeInTheDocument();
 });
--- src/helpers/electron.js
+++ src/helpers/electron.js
@@ -1,4 +1,21 @@
-const electron = window.require('electron');
+let electron;
+
+if(window && window.require) {
+	electron = window.require('electron');
+} else  { // NodeJS
+	electron = {
+		ipcRenderer: {
+			invoke: (event) => {
+				switch(event) {
+					case 'i18n:getTranslation':
+						return {}
+					default:
+						return [];
+				}
+			}
+		}
+	}
+}
 
 export default electron;
 export const ipcRenderer = electron.ipcRenderer;
