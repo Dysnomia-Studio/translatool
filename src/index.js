cca793bb3244bc97e9b3e392f14e6e8a5f96ad41
Elanis <elanis@hotmail.com>
2022-06-11 15:34:20 +0200

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 import ConfigSelector from './components/ConfigSelector';
 
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
 	<React.StrictMode>
 		<ConfigSelector />
 		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
