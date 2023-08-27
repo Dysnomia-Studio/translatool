0602500b30826915c849901d77c8d2a22bf5fb75
Elanis <elanis@hotmail.com>
2023-08-27 14:57:28 +0200

    Add a way to create a new key in translation files

--- src/components/LanguageComparator/view.jsx
+++ src/components/LanguageComparator/view.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+
 import TranslationList from '../TranslationList';
+
+import saveTranslation from '../../events/saveTranslation';
+
+import './index.css';
 
 export default function LanguageComparator({
 	useTranslation,
@@ -9,11 +15,32 @@ export default function LanguageComparator({
 	setRightLanguage,
 	selectedFolder
 }) {
+	const [inputText, setInputText] = useState('');
+
 	const leftTranslations = useTranslation(selectedFolder, currentFile, leftLanguage);
 	const rightTranslations = useTranslation(selectedFolder, currentFile, rightLanguage);
 
 	return (
 		<>
+			<div className="translation-key-add">
+				<input
+					type="text"
+					placeholder="New translation key"
+					value={inputText}
+					onChange={(e) => setInputText(e.target.value)}
+				/>
+				<input type="button" value="Add key" onClick={async() => {
+					try {
+						leftTranslations[inputText] = '';
+						rightTranslations[inputText] = '';
+						await saveTranslation(selectedFolder, currentFile, leftLanguage, leftTranslations);
+						setInputText('');
+					} catch(e) {
+						console.error(e);
+					}
+				}} />
+			</div>
+
 			<TranslationList
 				file={currentFile}
 				list={leftTranslations}
